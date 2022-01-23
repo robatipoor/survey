9acadd96ab8350c3ed5cb440e5b5f554af82befe
robatipoor <mahdi.robatipoor@gmail.com>
2022-01-23 08:41:36 +0330

    fix bug answer mapper

--- src/main/java/com/snap/survey/mapper/AnswerMapper.java
+++ src/main/java/com/snap/survey/mapper/AnswerMapper.java
@@ -4,11 +4,14 @@ import com.snap.survey.entity.AnswerEntity;
 import com.snap.survey.model.request.SubmitAnswerRequest;
 import com.snap.survey.model.response.AnswerResponse;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public abstract class AnswerMapper {
   public abstract AnswerEntity toEntity(SubmitAnswerRequest request);
 
-  // TODO mapper fix
+  @Mapping(source = "answer.user.id", target = "userId")
+  @Mapping(source = "answer.question.id", target = "questionId")
+  @Mapping(source = "answer.id", target = "answerId")
   public abstract AnswerResponse toResponse(AnswerEntity answer);
 }
